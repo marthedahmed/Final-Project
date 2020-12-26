dbcef63f387558d37072a04db6ab3fd482bc2206
wad-babiker <62028710+wadbabiker@users.noreply.github.com>
2020-12-26 21:30:34 +0200

    Add files via upload
    Added final university project

---
+++ contact.cpp
@@ -0,0 +1,151 @@
+/*
+    Name : Mohamed Babiker Osman
+    Computer Engineering
+    Second Class 
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct node
+{
+    string firstName;
+    string lastName;
+    string phoneNumber ;
+    string contactEmail ;
+    node *next;
+};
+
+class List
+{
+private:
+    node *head, *tail;
+public:
+    List()
+    {
+        head = NULL;
+        tail = NULL;
+    }
+
+    void create_contact(string first, string last , string num ,string em) // Create node and add it onto the linked list
+    {
+        node *temp = new node;
+        temp ->firstName = first;
+        temp ->lastName = last;
+        temp ->phoneNumber = num ;
+        temp ->contactEmail = em ;
+        temp->next = NULL;
+        if (head == NULL)
+        {
+            head = temp;
+            tail = temp;
+            temp = NULL;
+        }
+        else
+        {
+            tail->next = temp;
+            tail = temp;
+        }
+    }
+
+    void display_all() // Prints out current trip
+    {
+        int contactNum = 1;
+        node *temp = new node;
+        temp = head;
+        while (temp != NULL)    // Loop through the list while the temporary node is not empty
+        {
+            cout << "\nContact Number : " << contactNum << endl;
+            cout << "First Name : " << temp->firstName << endl;
+            cout << "Last Name : " << temp->lastName << endl;
+            cout<< "Phone Number : "<<temp->phoneNumber<<endl ;
+            cout<<"Contact Email : "<<temp->contactEmail<<endl ;
+            ++contactNum;
+            temp = temp->next;
+        }
+    }
+
+    void delete_position(int pos)   // delete a stop by using the position in the list
+    {
+        node *current = new node;
+        node *previous = new node;
+        node *next = new node;
+        current = head;
+        for (int i = 1;i<pos;i++)   // Loop through the link list while the current node is not empty
+        {
+            if (current == NULL)
+                return;
+            previous = current;
+            current = current->next;
+        }
+        next = current->next;
+        previous->next = current->next;
+        delete current;
+    }
+
+    void delete_head()  // delete head node
+    {
+        node *temp = new node;
+        temp = head;
+        head = head->next;
+        delete temp;
+    }
+};
+
+int main()
+{
+    List Contacts; // create a Contacts item for the List class
+    int choice, position;
+    string firstName;
+    string lastName;
+    string phoneNum ;
+    string email ;
+
+    while (1) {
+        cout << "\n===== Minu ===== " << endl;
+        cout << "1. Show All Contacts" << endl;
+        cout << "2. Add New Contact" << endl;
+        cout << "3. Remove Contact" << endl;
+        cout << "4. Edit Contact" << endl;
+        cout << "5. Exit The Program" << endl;
+        cout<<"Enter the Number Of Opration.....";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            cout << endl;
+            Contacts.display_all(); // display all contacts
+            cout << endl;
+            break;
+        case 2:
+            cout << "\nEnter First Name: ";
+            cin >> firstName;
+            cout << "\nEnter Last Name: ";
+            cin >> lastName;
+            cout<<"\nEnter Phone Number :";
+            cin>>phoneNum ;
+            cout<<"\nEnter Email : ";
+            cin>>email ;
+            Contacts.create_contact(firstName, lastName,phoneNum ,email); // create the contact in the linked list
+            cout << endl;
+            break;
+        case 3:
+            cout << "Enter the contact number of the contact you would like to remove: ";
+            cin >> position;
+            if (position == 1)
+                Contacts.delete_head();
+            else
+                Contacts.delete_position(position); // delete contact from list
+            break;
+        case 5:
+            exit(1);
+            break;
+        default:
+            cout << "\n" << choice << " is not an option. Please select a valid option." << endl;
+            break;
+        }
+    }
+    return 0;
+}
